3b1ea15028aca76b4a3187a79a50ae6c802ca9a7
Francisco Ossian <42554801+FranciscoOssian@users.noreply.github.com>
2022-05-12 15:23:21 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import React, { useRef, useState, useEffect } from 'react'
+import './App.css'
+import { initializeApp } from "firebase/app"
+import { getDatabase, ref, set, onValue } from "firebase/database"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBY83AUss0hPuuNAam6WFljEPJYCB7GZSM",
+  authDomain: "labcomp-finder.firebaseapp.com",
+  projectId: "labcomp-finder",
+  storageBucket: "labcomp-finder.appspot.com",
+  messagingSenderId: "1065990617790",
+  appId: "1:1065990617790:web:73f4d3a6192b0e93c6bcef",
+  measurementId: "G-LF3SZXCX8P"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const Computer = ({id, onClick, state}) =>
+  <li
+    className="computer"
+    state={state===1? 'on' : 'off'}
+    onClick={onClick}
+    id={id}>
+    💻
+  </li>
+
+const stdtState = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+function App() {
+
+  const inputEl = useRef(null)
+
+  const [computersState, setComputersState] = useState(stdtState)
+
+  async function writeStateData(state) {
+    const db = getDatabase()
+    return set(ref(db, 'computerState/'), state)
+  }
+
+  useEffect(()=>{
+    const run = async () => {
+      const db = getDatabase()
+      const stateRef = ref(db, 'computerState/')
+      onValue( stateRef, (snapshot) => {
+        const data = snapshot.val()
+        if(!data) setComputersState(stdtState)
+        else setComputersState(data)
+      })
+    }
+    run()
+  },[])
+
+
+  const turn = async (computer) => {
+    console.log(computer)
+    const s = [...computersState]
+    s[computer] = s[computer]===1? 0 : 1
+    setComputersState( s )
+    console.log(s)
+    await writeStateData(s);
+  }
+  
+  return (
+    <main>
+      <ul>
+        {
+          computersState
+          .map((computer, i) => 
+            <Computer
+              key={i}
+              onClick={ ()=>turn(i) }
+              id={i}
+              state={computersState[i]}
+            />
+          )
+        }
+      </ul>
+    </main>
+  );
+}
+
+export default App;
